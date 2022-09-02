f5676faa7d987cadabe8db2df4def948030b2975
Duje Buljat <dujebuljat1@gmail.com>
2022-09-02 22:51:04 +0200

    napravljene promjene u app fileu

--- app.py
+++ app.py
@@ -5,32 +5,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/Templates/index')
 def index():
     title = "Merchandise"
-    return render_template("index.html", title=title)
+    return render_template("Templates/index.html", title=title)
 
-@app.route('/Atlanta Hawks')
+@app.route('/Templates/Atlanta Hawks')
 def Atlanta_Hawks():
     title="Atlanta Hawks"
-    return render_template("Atlanta Hawks.html", title=title)
+    return render_template("Templates/Atlanta Hawks.html", title=title)
 
-@app.route('/Boston Celtics')
+@app.route('/Templates/Boston Celtics')
 def Boston_Celtics():
     title="Boston Celtics"
-    return render_template("Boston Celtics.html", title=title)
+    return render_template("Templates/Boston Celtics.html", title=title)
 
-@app.route('/Chicago Bulls')
+@app.route('/Templates/Chicago Bulls')
 def Chicago_Bulls():
     title="Chicago Bulls"
-    return render_template("Chicago Bulls.html", title=title)
+    return render_template("Templates/Chicago Bulls.html", title=title)
 
-@app.route('/Golden State')
+@app.route('/Templates/Golden State')
 def Golden_State():
     title="Golden State"
-    return render_template("Golden State.html", title=title)
+    return render_template("Templates/Golden State.html", title=title)
 
-@app.route('/L.A.Lakers')
+@app.route('/Templates/L.A.Lakers')
 def LA_Lakers():
     title="L.A.Lakers"
-    return render_template("L.A.Lakers.html", title=title)
+    return render_template("Templates/L.A.Lakers.html", title=title)
