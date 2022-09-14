44c77472f548bd6bd5dea989c2a3be871854471b
Duje Buljat <dujebuljat1@gmail.com>
2022-09-14 17:00:48 +0200

    pocetak integracije javascripta

---
+++ static/js/main.js
@@ -0,0 +1,56 @@
+// SELECT ELEMENTS
+const productsEl = document.querySelector(".card");
+const cartItemsEl = document.querySelector(".card");
+
+// RENDER PRODUCTS
+function renderProducts(){
+    products.forEach( (product) => {
+        productsEl.innerHTML += `
+        <div class="card">
+        <img class="card-img-top" src="${product.imgSrc}" alt="${product.name}" width="200" height="250">
+
+        <div class="card-body" >
+          <p class="card-text">${product.name}</p>
+          <div class="d-flex justify-content-between align-items-center">
+            <div onclick="addToCart(${product.id})" class="btn-group">
+              <button type="button" class="btn btn-sm btn-outline-secondary">Add to Cart</button>
+            </div>
+            <small class="text-muted"></small>
+          </div>
+        </div>
+        </div>
+        `
+    })
+}
+renderProducts();
+
+// cart array
+let cart = [];
+
+// ADD TO CART
+function addToCart(id){
+    // check if product already exists in cart
+    if(cart.some((item) => item.id === id)){
+        alert("Product already in cart!")
+    } else {
+        const item = products.find((product) => product.id === id);
+
+        cart.push({
+            ...item,
+            numberOfUnits : 1,
+        });
+    }
+
+    updateCart();
+}
+
+// update cart
+function updateCart(){
+    renderCartItems();
+    // renderSubTotal();
+}
+
+// render cart items
+function renderCartItems(){
+    cart.forEach(())
+}
